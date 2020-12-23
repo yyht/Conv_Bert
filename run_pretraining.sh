36b82b370257dee1c5865a98ce175f79484cfefa
天逸 <albert.xht@alibaba-inc.com>
2020-12-23 11:13:57 +0800

    add pretraining script

---
+++ run_pretraining.sh
@@ -0,0 +1,5 @@
+nohup python run_pretraining.py \
+	--data-dir gs://yyht_source/pretrain \
+	--data-file-list chinese_simplified_whole_sentence_v3_32/chinese_simplified_whole_sentence_file.txt \
+	--model-name electria_conv_bert_base \
+	--hparams electria_base_params.json
