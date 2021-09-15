c2a17990635593baff99037c821220f81e1d6f62
天逸 <htxu91@gmail.com>
2021-09-15 09:01:16 +0800

    add conv-bert-electra

--- run_pretraining.py
+++ run_pretraining.py
@@ -451,7 +451,7 @@ def main():
     hparams = utils.load_json(args.hparams)
   else:
     hparams = json.loads(args.hparams)
-  tf.logging.set_verbosity(tf.logging.ERROR)
+  # tf.logging.set_verbosity(tf.logging.ERROR)
   train_or_eval(configure_pretraining.PretrainingConfig(
       args.model_name, args.data_dir, args.data_file_list, **hparams))
 
